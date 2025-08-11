ada65ec3f9c7a3bbc515dd8aef5d4c9915f21e02
tgrozenski <tyler.grozenski@gmail.com>
2025-08-11 10:50:32 -0700

    added callback

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const loginButton = document.getElementById('login-btn');
-if (loginButton) {
-    loginButton.addEventListener('click', () => {
-        window.location.href =
-            'https://tgrozenski.github.io/agent_email_frontend.github.io/login.html';
-    });
-}
-
 const oAuthButton = document.getElementById('oAuth-btn');
 if (oAuthButton) {
     oAuthButton.addEventListener('click', () => {

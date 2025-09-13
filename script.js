fb6a1f85f07544a009b7e05004ee177417245be5
tgrozenski <tyler.grozenski@gmail.com>
2025-09-12 22:10:50 -0700

    cleaned up project

--- script.js
+++
@@ -1,6 +0,0 @@
-const oAuthButton = document.getElementById('oAuth-btn');
-if (oAuthButton) {
-    oAuthButton.addEventListener('click', () => {
-        console.log('Google button clicked!');
-    });
-}
